cbf87f156bc70e3389571b5ae415091ad2708f94
Nainesh <naineshjethwa@gmail.com>
2017-11-01 22:21:29 -0400

    Spoiler is now running perfectly

--- Spoliers/ViewController.swift
+++ Spoliers/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+      let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            
+            
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                
+                fatalError("Failed to convert image")
+            }
+            
+            detect(image: ciimage)
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    func detect(image:CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model:model) { (request, error) in
+         
+            guard  let result = request.results as? [VNClassificationObservation] else {
+            
+            fatalError("Failed to request")
+            }
+            if let firstResult = result.first{
+                if firstResult.identifier.contains("Pizza"){
+                    self.navigationItem.title = "Pizza!"
+                    
+                }else {
+                    self.navigationItem.title = "Not Pizza!"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+        try! handler.perform([request])
+        }
+        catch {
+            print(error)
+            
+        }
+    }
+    
+    @IBAction func camera(_ sender: Any) {
+        
+        
+         present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
 
 }
 
